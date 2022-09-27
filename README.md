3ba710575274b4822e75921964e14118fcca6d26
nikhil-yadav999 <nikhil919yadav@gmail.com>
2022-09-27 22:06:23 +0530

    3 commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# project1
+This project is a semi clone of websites like geeks for geeks,w3schools,etc
+The content is not synchronous and is random ,its just for demonstration of the page layout ,styling ,it is to be edited shortly
+Html,scss,css,javasript are used in the project
+bootstarp is also used in the project whereas javascript is kept minimum
+this project has been built using npm ,parcel builder tool
+this is just a prototype and a lot of changes are yet to be made in the project
---
+++ src/assets/js/main.js
@@ -0,0 +1,110 @@
+import 'bootstrap';
+
+
+rightbar.addEventListener('mouseover',function(){
+    let bg=document.getElementById("rightbar");
+    bg.style.background="green";
+    console.log("is this working just fine");
+});
+/*rightbar.addEventListener('mouseup',function(){
+    console.log("this is working too i guess no");
+    let bg=document.getElementById("rightbar");
+    bg.style.background="yellow";
+})*/
+rightbar.addEventListener('mouseout',function(){
+    console.log("this too!");
+    let bg=document.getElementById("rightbar");
+    bg.style.background="blueviolet";
+});
+/*rightbar.addEventListener('mousedown',function(){
+    console.log("this is working too i guess no");
+    let bg=document.getElementById("rightbar");
+    bg.style.background="brown";
+})*/
+let g=document.getElementsByClassName("gi");
+
+let prev=document.getElementById("rightbar").innerText;
+//didn't work with only g.addev......
+g[0].addEventListener('click',function(){
+    console.log("voila");
+    //document.getElementsByClassName("gi");
+    g[0].style.background="red";
+    document.getElementById("rightbar").innerText="Wohoo!!!";
+});
+g[1].addEventListener('click',function(){
+    console.log("voila");
+    //document.getElementsByClassName("gi");
+    g[0].style.background="green";
+    document.getElementById("rightbar").innerText=prev;
+});
+
+//arrow notation
+summ=(a,b)=>{
+    //return a+b;
+    console.log(a+b);
+}
+
+//clr=setInterval(diff,3000);
+//let z=0;
+diff=()=>{
+    //z++;
+    console.log("jsahdbj");
+    document.getElementById("rightbar").innerText="Yowza!!";
+    //if(z==3)
+    //{
+      //  clearInterval(clr);
+    //}
+}
+/*setTimeout(summ(5,8),50000);
+setTimeout(diff,3000);*/
+//setInterval(diff,3000);
+//clr=setInterval(diff,3000);
+
+/*localStorage.setItem('name','nikhil');
+localStorage.getItem('name');
+localStorage.removeItem('name');
+localStorage.clear();
+obj={
+    name:"nikhil",length:1,a:{this:"that"}
+}
+jso =JSON.stringify(obj);
+console.log(typeof jso);
+console.log(jso);
+parsed=JSON.parse(`{"name":"nikhil","length":1,"a":{"this":"that"}}`);
+console.log(parsed);*/ 
+let db=document.getElementsByTagName("button");
+
+for(let k=1;k<15;k++)
+{
+db[k].addEventListener('click',function(){
+    alert("These buttons are not usable as of now");
+})}
+
+/*function btnalert(){
+    alert("These buttons are not usable as of now");
+}*/
+let slideIndex=0;
+//let c=0;
+showslides();
+function showslides(){
+    let i;
+    let slides=document.getElementsByClassName("myslides");
+    for(i=0;i<slides.length;i++)
+    {
+        slides[i].style.display="none";
+    }
+    slideIndex++;
+    if(slideIndex>slides.length)
+    {
+        slideIndex=1;
+    }
+    slides[slideIndex-1].style.display="block";
+    setTimeout(showslides,2000);
+    /*if(c==6)
+    {
+        return;
+    }*/
+}
+
+let fl=document.getElementsByClassName("gi");
+//do the click button show content of flashcards
